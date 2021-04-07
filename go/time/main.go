e202057a1cabb47e488782d0067e984498fbc644
sanoyo <sa00380304yo@gmail.com>
2021-04-08 07:16:39 +0900

    time.After

--- go/time/main.go
+++ go/time/main.go
@@ -11,4 +11,11 @@ func main() {
 	// 10分前
 	sample = sample.Add(-10 * time.Minute)
 	fmt.Println(sample)
+
+	t1 := time.Date(2020, 10, 16, 23, 6, 40, 0, time.Local)
+	t2 := time.Date(2019, 10, 17, 23, 6, 40, 0, time.Local)
+	// t2よりもt1が以前の時刻かどうか
+	fmt.Println(t2.Before(t1))
+	// t2よりもt1が先の時刻かどうか
+	fmt.Println(t2.After(t1))
 }
