00c878f0544cd9385ef8eb45be8733f2ebbe7826
sanoyo <sa00380304yo@gmail.com>
2021-04-10 11:04:05 +0900

    regexp

---
+++ go/regex/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	str := "a[b+c=\\"
+	fmt.Println(str)
+	fmt.Println(regexp.QuoteMeta(str))
+
+	query := "UPDATE accounts SET password = $1, email = $2, updated_at = now() WHERE id = $3"
+	reQuery := regexp.QuoteMeta(query)
+	fmt.Println(query)
+	fmt.Println(reQuery)
+}
