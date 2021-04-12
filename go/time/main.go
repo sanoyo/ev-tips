6efee45e0799b1a72b698f18f70855a2d5a65c56
sanoyo <sa00380304yo@gmail.com>
2021-04-12 21:53:11 +0900

    Truncate

--- go/time/main.go
+++ go/time/main.go
@@ -18,4 +18,15 @@ func main() {
 	fmt.Println(t2.Before(t1))
 	// t2よりもt1が先の時刻かどうか
 	fmt.Println(t2.After(t1))
+
+	// 時間の切り捨て
+	// ref: https://qiita.com/tchnkmr/items/f3c94abb3e3a47e993ab
+	t := time.Date(2001, 2, 3, 4, 5, 6, 789000000, time.UTC)
+	fmt.Println(t)                          // -> 2001-02-03 04:05:06.789 +0000 UTC Truncate前
+	fmt.Println(t.Truncate(time.Hour * 24)) // -> 2001-02-03 00:00:00 +0000 UTC
+	fmt.Println(t.Truncate(time.Hour))      // -> 2001-02-03 04:00:00 +0000 UTC
+	fmt.Println(t.Truncate(time.Minute))    // -> 2001-02-03 04:05:00 +0000 UTC
+	fmt.Println(t.Truncate(time.Second))    // -> 2001-02-03 04:05:06 +0000 UTC
+	t = time.Date(2001, 2, 3, 4, 5, 6, 789000000, time.Local)
+	fmt.Println(t.Truncate(time.Hour * 24)) // -
 }
