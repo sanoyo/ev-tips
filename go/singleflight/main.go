671b02fbe22e379f74e9c0fba0438f8aef3eda2e
sanoyo <sa00380304yo@gmail.com>
2021-07-18 17:18:16 +0900

    Revise singleflight comment

--- go/singleflight/main.go
+++ go/singleflight/main.go
@@ -1,36 +1,55 @@
+// https://christina04.hatenablog.com/entry/go-singleflight
 package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"sync"
-	"time"
 
 	"golang.org/x/sync/singleflight"
 )
 
 var group singleflight.Group
 
-func doSomething(name string) {
-	v, err, shared := group.Do(name, func() (interface{}, error) {
-		time.Sleep(10 * time.Millisecond)
-		return time.Now(), nil
+func download(path string) ([]byte, error) {
+	log.Println("download")
+	resp, err := http.Get(path)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func suppressDupCall(path string) ([]byte, error) {
+	v, err, _ := group.Do(path, func() (interface{}, error) {
+		return download(path)
 	})
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	fmt.Printf("result: %s, shared: %t\n", v, shared)
+	return v.([]byte), nil
 }
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+
+	path := "https://www.gstatic.com/webp/gallery3/1.png"
 	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			doSomething("hogehoge")
+			// img, err := download(path)
+			img, err := suppressDupCall(path)
+			fmt.Printf("result: %d, err: %v\n", len(img), err)
 		}()
-		time.Sleep(time.Millisecond)
 	}
 	wg.Wait()
 }
